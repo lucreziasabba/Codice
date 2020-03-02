6c92837f9883890192d80e0a5a90150c8035929d
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2020-03-02 09:50:23 +0100

    Add project files.

---
+++ ConsoleCodice/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+
+namespace ConsoleCodice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string file = "input.txt";
+            using(StreamReader sr=new StreamReader(file))
+            {
+                string num = sr.ReadLine();
+                int x=
+            }
+        }
+    }
+}
---
+++ LibreriaCodice/MetodiCodice.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace LibreriaCodice
+{
+    public class MetodiCodice
+    {
+    }
+}
